bb1da85e81c10c80733b319789f55ba254cc96ea
xg75Michael <xg75@students.waikato.ac.nz>
2019-10-15 13:52:10 +0800

    cp 3

---
+++ LiYanhuiJS/3-语法关键字保留字及变量/demo.js
@@ -0,0 +1,21 @@
+//var box = 100;
+// 声明变量并初始化
+// var box; 声明但没有赋值， 返回 undefined
+// var box = 200; 重复声明同一个变量只是赋值操作
+//alert(box);
+// 以弹窗的方式输出box的值
+// alert(something) 报错，因为没有声明something
+
+
+//var box = 'Michael'; var age = 28; alert(box); alert(age);
+// 可以写在一行，用分号分隔
+// 多行书写的时候建议添加分号
+
+
+var box = 'Michael',
+    age = 28,
+    height = 174;
+// var声明多个变量用 ， 分隔；
+alert(box);
+alert(age);
+alert(height);
