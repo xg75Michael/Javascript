06d39c2a1a0db2018dad13a76b1c969eb786be41
xg75Michael <xg75@students.waikato.ac.nz>
2019-12-04 09:37:14 +0800

    compatible way to add event

---
+++ AlgorithmScripting/CompatibleAddEvent.js
@@ -0,0 +1,26 @@
+var EventUtil = {
+	addHandler: function(element, type, handler) {
+		if (element.addEventListener) {
+			element.addEventListener(type, handler, false);
+		} else if (element.attachEvent) {
+			element.attachEvent('on' + type, handler);
+		} else {
+			element['on' + type] = handler;
+		}
+	},
+	removeHandler: function(element, type, handler) {
+		if (element.removeEventListener) {
+			element.removeEventListener(type, handler, false);
+		} else if (element.detachEvent) {
+			element.detachEvent('on' + type, handler);
+		} else {
+			element['on' + type] = handler;
+		}
+	}
+};
+
+// 调用的例子
+function someHandler() {
+	console.log('clicked');
+}
+EventUtil.addHandler(document, 'click', someHandler);
